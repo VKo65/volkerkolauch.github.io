13fbfdfaae8d27bb024e3f4f3ed6d656bc804f83
VKo65 <volker@kolauch.de>
2025-08-25 12:33:25 +0200

    Set site URL to https://vko65.github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import github from "@astrojs/github/actions";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://VKo65.github.io',
+  integrations: [github()],
+});
