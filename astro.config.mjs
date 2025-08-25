c61fb7c2d80768b5d174b0fd36566f94566a11fb
VKo65 <volker@kolauch.de>
2025-08-25 14:58:18 +0200

    Publish static site to root

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
-import github from '@astrojs/github/actions';
 
 export default defineConfig({
-  site: 'https://vko65.github.io', // wichtig: dein Nutzername = VKo65 → Domain = vko65.github.io
-  integrations: [github()],
+  site: 'https://vko65.github.io',
 });
